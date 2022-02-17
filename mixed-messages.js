d5de22b54af81071a739f274ad33f36c498b0bc6
Icy-Rex <gabebigelow@gmail.com>
2022-02-16 21:56:35 -0500

    Add poor/netural/good fortune array and luck variables

---
+++ mixed-messages.js
@@ -0,0 +1,8 @@
+let poorFortunes = [];
+let neutralFortunes = [];
+let goodFortunes = [];
+
+let morningLuck = Math.random().toFixed(2);
+let afternoonLuck = Math.random().toFixed(2);
+let nightLuck = Math.random().toFixed(2);
+
